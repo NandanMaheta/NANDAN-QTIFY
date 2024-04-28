f5bee4ada2e559261fe54c634cd254e66481aa60
NandanMaheta <nandzmehta99@gmail.com>
2024-04-29 00:38:44 +0530

    solved a bug with navbar styling

--- qtify/src/components/NavBar.jsx
+++ qtify/src/components/NavBar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import logo from "../assests/LOGO.png"
+import logo from "../assests/LOGO.png";
 import FeedBack from "./FeedBack";
-import styles from "./NavBar.module.css"
-import theme from "./ThemeProvider";
-import { useTheme } from '@mui/material/styles'; 
-function NavBar(){
-    const muiTheme = useTheme();
-    return(
-        <div className={styles.NavBarFlex} style={{ backgroundColor: muiTheme.palette.primary.main, }}>
-        <img src={logo} alt="logo"/>
-        <SearchBar/>
-        <FeedBack/>
-        </div>
-    )
+import styles from "./NavBar.module.css";
+import { useTheme } from '@mui/material/styles';
+
+function NavBar() {
+  const muiTheme = useTheme();
+  return (
+    <div className={styles.NavBarFlex} style={{ backgroundColor: muiTheme.palette.primary.main }}>
+      <div className={styles.LogoContainer}>
+        <img src={logo} alt="logo" className={styles.Logo} />
+      </div>
+      <SearchBar />
+      <FeedBack />
+    </div>
+  );
 }
+
 export default NavBar;
